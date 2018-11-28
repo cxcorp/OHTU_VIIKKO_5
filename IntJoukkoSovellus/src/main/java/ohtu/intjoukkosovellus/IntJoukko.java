1616394cb15cb1a379305a21f3bd76e301fff626
Joona Heikkilä <joona.o.heikkila@gmail.com>
2018-11-28 15:19:27 +0200

    Reorganize IntJoukko methods

--- IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
+++ IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
@@ -48,24 +48,6 @@ public class IntJoukko {
         return true;
     }
 
-    private void lisaaLukutaulukonPeralle(int luku) {
-        lukutaulukko[alkioidenMaara] = luku;
-        alkioidenMaara++;
-    }
-
-    private boolean lukutaulukkoOnTaynna() {
-        return alkioidenMaara == lukutaulukko.length;
-    }
-
-    private void kasvataLukutaulukkoa() {
-        int uusiKoko = laskeKasvatettuKoko();
-        lukutaulukko = Arrays.copyOf(lukutaulukko, uusiKoko);
-    }
-
-    private int laskeKasvatettuKoko() {
-        return alkioidenMaara + kasvatuskoko;
-    }
-
     public boolean kuuluu(int etsittavaLuku) {
         return indexOf(etsittavaLuku) != -1;
     }
@@ -79,6 +61,58 @@ public class IntJoukko {
         poistaTaulukosta(index);
         alkioidenMaara--;
         return true;
+    }
+
+    public int mahtavuus() {
+        return alkioidenMaara;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{%s}", lukutaulukkoStringiksi());
+    }
+
+    public int[] toIntArray() {
+        return Arrays.copyOf(lukutaulukko, alkioidenMaara);
+    }
+
+    public static IntJoukko yhdiste(IntJoukko a, IntJoukko b) {
+        IntJoukko uusiJoukko = new IntJoukko();
+        lisaaKaikki(uusiJoukko, a.toIntArray());
+        lisaaKaikki(uusiJoukko, b.toIntArray());
+        return uusiJoukko;
+    }
+
+    public static IntJoukko leikkaus(IntJoukko a, IntJoukko b) {
+        IntJoukko uusiJoukko = new IntJoukko();
+        lisaaMolemmissaOlevat(a, b, uusiJoukko);
+        return uusiJoukko;
+    }
+
+    public static IntJoukko erotus(IntJoukko a, IntJoukko b) {
+        IntJoukko uusiJoukko = yhdiste(a, b);
+        poistaKaikki(uusiJoukko, b.toIntArray());
+        return uusiJoukko;
+    }
+
+    private static void poistaKaikki(IntJoukko joukko, int[] luvut) {
+        for (int luku : luvut) {
+            joukko.poista(luku);
+        }
+    }
+
+    private static void lisaaMolemmissaOlevat(IntJoukko a, IntJoukko b, IntJoukko kohde) {
+        for (int luku : a.toIntArray()) {
+            if (b.kuuluu(luku)) {
+                kohde.lisaa(luku);
+            }
+        }
+    }
+
+    private static void lisaaKaikki(IntJoukko joukko, int[] luvut) {
+        for (int luku : luvut) {
+            joukko.lisaa(luku);
+        }
     }
 
     private void poistaTaulukosta(int indeksi) {
@@ -99,60 +133,26 @@ public class IntJoukko {
         );
     }
 
-    public int mahtavuus() {
-        return alkioidenMaara;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("{%s}", lukutaulukkoStringiksi());
-    }
-
     private String lukutaulukkoStringiksi() {
         return Joiner.on(", ").join(Ints.asList(toIntArray()));
     }
 
-    public int[] toIntArray() {
-        return Arrays.copyOf(lukutaulukko, alkioidenMaara);
+    private void lisaaLukutaulukonPeralle(int luku) {
+        lukutaulukko[alkioidenMaara] = luku;
+        alkioidenMaara++;
     }
 
-    public static IntJoukko yhdiste(IntJoukko a, IntJoukko b) {
-        IntJoukko uusiJoukko = new IntJoukko();
-        lisaaKaikki(uusiJoukko, a.toIntArray());
-        lisaaKaikki(uusiJoukko, b.toIntArray());
-        return uusiJoukko;
+    private boolean lukutaulukkoOnTaynna() {
+        return alkioidenMaara == lukutaulukko.length;
     }
 
-    private static void lisaaKaikki(IntJoukko joukko, int[] luvut) {
-        for (int luku : luvut) {
-            joukko.lisaa(luku);
-        }
+    private void kasvataLukutaulukkoa() {
+        int uusiKoko = laskeKasvatettuKoko();
+        lukutaulukko = Arrays.copyOf(lukutaulukko, uusiKoko);
     }
 
-    public static IntJoukko leikkaus(IntJoukko a, IntJoukko b) {
-        IntJoukko uusiJoukko = new IntJoukko();
-        lisaaMolemmissaOlevat(a, b, uusiJoukko);
-        return uusiJoukko;
-    }
-
-    private static void lisaaMolemmissaOlevat(IntJoukko a, IntJoukko b, IntJoukko kohde) {
-        for (int luku : a.toIntArray()) {
-            if (b.kuuluu(luku)) {
-                kohde.lisaa(luku);
-            }
-        }
-    }
-
-    public static IntJoukko erotus(IntJoukko a, IntJoukko b) {
-        IntJoukko uusiJoukko = yhdiste(a, b);
-        poistaKaikki(uusiJoukko, b.toIntArray());
-        return uusiJoukko;
-    }
-
-    private static void poistaKaikki(IntJoukko joukko, int[] luvut) {
-        for (int luku : luvut) {
-            joukko.poista(luku);
-        }
+    private int laskeKasvatettuKoko() {
+        return alkioidenMaara + kasvatuskoko;
     }
 
     private int indexOf(int etsittavaLuku) {
